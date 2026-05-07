6157a1e249390241c8b7f44d6a31d949b1d55d8b
Ivan Podkolzin <nizlokdop@gmail.com>
2026-05-07 14:27:58 +0300

    p

--- angular_proc.R
+++ angular_proc.R
@@ -4,18 +4,23 @@ devtools::load_all()
 df <- load_trace(path_dir = "C:/RWD/Angular_Sim_Exp/GNR_060/TRACES")|>
   tr_crop(crop_to = c(5, 110))|>
   #tr_resample(pts = 800)|>
-  tr_rescale(type = "minmax")|>
-  add_field(new_field = list(Group = "Alteplase"))
+  tr_rescale(type = "minmax")
+
 
 df$META <- lapply(df$META, function(dt){
   
-  dt|> dplyr::mutate(Group = dplyr::if_else(
-    grepl(SampleName, pattern = "060"), "Tenekteplase", "Alteplase"))
+  dt$SampleName <- gsub(dt$SampleName, pattern = "GNR_060", replacement =  "Tenecteplase")
+  dt$SampleName <- gsub(dt$SampleName, pattern = "GNR_004", replacement =  "Alteplase")
+  
+  dt <- dt|> dplyr::mutate(Group = dplyr::if_else(
+    grepl(SampleName, pattern = "Tenecteplase"), "Tenecteplase", "Alteplase"))
+
+  dt
 })
 
 
 idx <- trace_info(df)|>
-  dplyr::filter(grepl(Channel.Description, pattern = "280"))|>
+  dplyr::filter(grepl(Channel.Description, pattern = "215"))|>
   dplyr::pull(ID)
 
 
@@ -27,10 +32,11 @@ plt_gg(df
        , facet_lab = "SampleName"
        , force_raw = F
        , gr_col = "Group"
+       , what = c(1,4)
 )
 
 
-compar <- tr_compar(x = b
+compar <- tr_compar(x = df
                     , metric = "cosdist"
                     , pw = 0.55
                     , lab = "SampleName")
@@ -38,29 +44,29 @@ hc <- compar$SIM |>
   as.dist()|>
   hclust(method = "ward.D2")
 
-par(mar=c(2,2,3,6))
+par(mar=c(8,2,2,2))
 
 COLS <- c("red", "blue","purple", "magenta")
+K <- 2
 
 dend <- hc |>
   as.dendrogram() |>
   dendextend::set("labels_cex", value = 0.8) |>
-  dendextend::set("labels_col", value = COLS, k = 4) |>
-dendextend::set("branches_k_color", value = COLS, k = 4) |>
-plot(horiz = TRUE, axes = F)
+  dendextend::set("labels_col", value = COLS[1:K], k = K) |>
+dendextend::set("branches_k_color", value = COLS[1:K], k = K) |>
+plot(horiz = F, axes = F)
 
 
 compar$SIM |> corrplot::corrplot(method = c("square")
                                  , type = "lower"
                                  , diag = F
-                                 , addCoef.col = "black"
                                  , is.corr = F
                                  , order = "hclust"
                                  , hclust.method = "ward.D2"
-                                 , col = corrplot::COL1('Oranges', 100)
-                                 #, col.lim = c(0.4, 1)
+                                 , col = rev(hcl.colors(80, "viridis"))
+                                 , addCoef.col = "grey45"
+                                 #, bg = "lightblue"
                                  )
-
 
 
 # ============================= GNR - 045 =============================
@@ -99,17 +105,36 @@ df$META <- lapply(df$META, function(dt){
 })
 
 
+df$META <- lapply(df$META, function(dt){
+  
+  dt$SampleName <- gsub(dt$SampleName, pattern = "GNR-045", replacement =  "Eculizumab")
+  dt$SampleName <- gsub(dt$SampleName, pattern = "GNR-118", replacement =  "Ravulizumab")
+  
+  dt <- dt|> dplyr::mutate(Group = dplyr::case_when(
+    grepl(SampleName, pattern = "Ecul") ~ "Eculizumab",
+    .default = "Ravulizumab"))|> 
+    
+    dplyr::mutate(Group = dplyr::case_when(
+      grepl(SampleName, pattern = "]$") ~ "Oxidized",
+      .default = Group))
+  dt
+  })
+
+
+
+
 trace_info(df)|>
   dplyr::arrange(dateAcquired)|>
   dplyr::select(dateAcquired, SampleName)
 
 
 
-plt_gg(b
+plt_gg(df
        , facet_lab = "SampleName"
        , stacked = T
        , force_raw = F
-       #, gr_col = "Group"
+       , gr_col = "Group"
+       , what = c(1,4,7,8,9,10,11)
 )
 
 
@@ -120,7 +145,7 @@ b <- merge_trace(a, df)
 
 b <- merge_trace(b, df)
 
-trace_info(b)
+trace_info(df)
 
 b$META <- lapply(b$META, function(dt){
   
@@ -131,10 +156,15 @@ b$META <- lapply(b$META, function(dt){
 })
 
 saveRDS(object = b, file = "I:/Angular_Sim_Exp/GNR_045_COSIM/GNR_045vs118.Rds")
+df <- readRDS(file = "F:/Angular_Sim_Exp/GNR_045_COSIM/GNR_045vs118.Rds")
 
+dat <- compar$WM |>
+  dplyr::filter(Pair == "Tenecteplase_2_vs_Alteplase_2")
 
-
-
-
+ggplot2::ggplot(data = dat, ggplot2::aes(x=RT, y = W))+
+  ggplot2::geom_line(col = "#bc42f5")+
+  ggplot2::geom_vline(xintercept = dat$RT)+
+  ggplot2::theme_bw()+
+  ggplot2::theme(panel.grid = ggplot2::element_blank())
 
 
