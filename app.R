0774609d21ebe9dd94dc0cad32f558113c128c68
IvanPodkolzin <nizlokdop@gmail.com>
2026-07-07 21:50:18 +0300

    upd

--- app.R
+++ app.R
@@ -13,16 +13,48 @@ ui <- fluidPage(
               , multiple = TRUE
               , accept = c(".csv", ".arw", ".txt")
               , placeholder = "browse a file"),
+    h3("UPLOAD LOG"),
+    DT::DTOutput("log"),
+    plotOutput("plot")
+  )
     
-    tableOutput("files")
-    )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
-  output$files <- renderTable(input$upload)
+  data <- reactive({
+    
+    req(input$upload)
+    obj <- traceR::load_trace(fls = input$upload$datapath)
+    obj$LOG$FILE_NAME <- input$upload$name
+    obj$LOG |> dplyr::select(!FILE)
+    
+    
+  })
+  
+  output$log <- DT::renderDT({
+    
+    DT::datatable(data()
+                  , filter = "none"
+                  , rownames = TRUE
+                  , options = list( 
+                    scrollY = "200px",
+                    scrollX = TRUE,
+                    paging = FALSE)
+                  )
+    
+    })
+  
+  output$plot <- renderPlot({
+    
+    traceR::plt_gg(x = )
+    
+  })
+  
   
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
