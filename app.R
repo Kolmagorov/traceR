5a0aa6d299bba8e5886a775566b1ce0109ad84ab
IvanPodkolzin <nizlokdop@gmail.com>
2026-07-08 21:09:34 +0300

    upd

--- app.R
+++ app.R
@@ -13,9 +13,33 @@ ui <- fluidPage(
               , multiple = TRUE
               , accept = c(".csv", ".arw", ".txt")
               , placeholder = "browse a file"),
-    h3("UPLOAD LOG"),
-    DT::DTOutput("log"),
-    plotOutput("plot")
+    hr(),
+    h2("Preview LOG"),
+    DT::DTOutput("preview"),
+    actionButton(inputId = "submit_btn"
+                 , label = "Submit"
+                 , icon = icon("stats", lib = "glyphicon")),
+    hr(),
+    h2("Traces"),
+    
+    selectInput(
+      inputId = "Scaling",
+      label = "Select a func.:",
+      choices = c("NONE" = "none", "MINMAX" = "minmax", "MAXNORM" = "maxnorm"),
+      selected = "none"
+    ),
+    
+    plotOutput("plot"),
+    sliderInput("time_rng", "Retention Time"
+                , value = c(0, 100)
+                , min = 0
+                , max = 100
+                , width = "100%"
+                , step = 1
+                , post = "min"),
+    
+    textOutput("txt")
+    
   )
     
 
@@ -23,21 +47,64 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
-  data <- reactive({
+  # Get preview data
+  prev_dt <- reactive({
     
     req(input$upload)
     obj <- traceR::load_trace(fls = input$upload$datapath)
     obj$LOG$FILE_NAME <- input$upload$name
-    obj$LOG |> dplyr::select(!FILE)
     
+    obj$LOG |>
+      dplyr::select(!c(FILE, SOURCE))|>
+      merge(traceR::trace_info(obj, force_raw = TRUE), by ="ID")
+
+  })
+  
+  observeEvent(input$submit_btn, {
+    
+    obj <- traceR::load_trace(fls = input$upload$datapath) # indexes are changed
+    what <- NULL
+    
+    if(!is.null(input$preview_rows_selected)){
+      
+      what <- prev_dt()[["ID"]][input$preview_rows_selected]
+      
+      obj <- traceR::copy_trace(x = obj, what = what)
+    }
+    
+    output$txt <- renderPrint(what)
     
   })
   
-  output$log <- DT::renderDT({
+  
+  
+  
+  
+  # Update sliderInput with uploaded data
+  #observeEvent(input$upload, {
     
-    DT::datatable(data()
+    # Extract Meta data table
+    #dt_meta <- traceR::trace_info(data(), force_raw = TRUE)
+    
+    # Set limits
+    #rng_min <- min(dt_meta$minRT, na.rm = TRUE)
+    #rng_max <- max(dt_meta$maxRT, na.rm = TRUE)
+    
+    # update Slider
+    #updateSliderInput(inputId = "time_rng"
+   #                   , session = session
+    #                  , min = rng_min
+    #                  , max = rng_max
+    #                  , value = c(rng_min, rng_max))
+ # })
+  
+  # Render Preview Data
+  output$preview <- DT::renderDT({
+    
+    DT::datatable(prev_dt()
                   , filter = "none"
                   , rownames = TRUE
+                  , selection = "multiple"
                   , options = list( 
                     scrollY = "200px",
                     scrollX = TRUE,
@@ -45,13 +112,23 @@ server <- function(input, output, session) {
                   )
     
     })
-  
-  output$plot <- renderPlot({
+  # Render Chromatograms
+  #output$plot <- renderPlot({
     
-    traceR::plt_gg(x = )
+    #obj <- data()|>
+      #traceR::tr_crop(crop_to = c(input$time_rng[1]
+                                  #,input$time_rng[2]))
     
-  })
+   # if(input$Scaling != "none"){
+     # obj <- traceR::tr_rescale(x = data(), type = input$Scaling)
+    #}
+    
+   # traceR::plt_gg(x = obj)
+    
+  #})
   
+  
+  #output$txt <- renderPrint(input$Scaling)
   
 }
 
