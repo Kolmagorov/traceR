d1937bd3943ec8f287fec6ef0736641ca3804299
Ivan Podkolzin <nizlokdop@gmail.com>
2026-08-04 17:19:02 +0300

    upd

--- Dash_UI.R
+++ Dash_UI.R
@@ -1,56 +1,64 @@
 library(shiny)
 library(bslib)
 
+# Sidebar Accordion ============================================================
+sb_acc <- accordion(id = "sb_accorion"
+                    , open = FALSE
+                    , accordion_panel(title = "IMPORT"
+                                      , icon = bsicons::bs_icon("database-up")
+                                      , fileInput("upload"
+                                                  , label = "Select a file:"
+                                                  , buttonLabel = "Upload..."
+                                                  , multiple = TRUE
+                                                  , accept = c(".csv", ".arw", ".txt")
+                                                  , placeholder = "browse a file"))
+                    
+                    , accordion_panel(title = "PROCESS"
+                                      , icon = bsicons::bs_icon("plus-slash-minus")
+                                      , sliderInput("time_rng", "Adjust sliders to crop out Retention time segment"
+                                                    , value = c(0, 100)
+                                                    , min = 0
+                                                    , max = 100
+                                                    , width = "100%"
+                                                    , step = 1
+                                                    , post = " min"))
+                    
+                    , accordion_panel(title = "ANALYSIS"
+                                      , icon = bsicons::bs_icon("puzzle")))
 
-acc <- accordion(
-  accordion_panel("UPLOAD"
-                  , icon = bsicons::bs_icon("files")
-                  , fileInput("upload"
-                              , label = "Select a file:"
-                              , buttonLabel = "Upload..."
-                              , multiple = TRUE
-                              , accept = c(".csv", ".arw", ".txt")
-                              , placeholder = "browse a file")),
-  
-  accordion_panel("PROCESS", icon = bsicons::bs_icon("plus-slash-minus")),
-  accordion_panel("ANALYSIS", icon = bsicons::bs_icon("puzzle"))
+nav_set <- navset_tab(
+  nav_panel(title = "PREVIEW"
+            , plotOutput("p")),
+  nav_panel(title = "META DATA EDITOR", p("Second tab content.")),
+  nav_spacer(),
+  nav_menu(
+    title = "Links",
+    nav_item("link_shiny"),
+    nav_item("link_posit")
+  )
 )
 
-ui <- page_sidebar(title = "UVizor"
-                   , sidebar = sidebar("Sidebar"
-                                       , id = "SD"
-                                       , open = "closed"
-                                       , resizable = FALSE
-                                       , accordion(id = "sb_accorion"
-                                                   , accordion_panel(title = "UPLOAD"
-                                                                     , fileInput("upload"
-                                                                                 , label = "Select a file:"
-                                                                                 , buttonLabel = "Upload..."
-                                                                                 , multiple = TRUE
-                                                                                 , accept = c(".csv", ".arw", ".txt")
-                                                                                 , placeholder = "browse a file")
-                                                    )
-                                                   , accordion_panel(title = "PROCESS"
-                                                                     , sliderInput("time_rng", "Adjust sliders to crop out Retention time segment"
-                                                                                   , value = c(0, 100)
-                                                                                   , min = 0
-                                                                                   , max = 100
-                                                                                   , width = "100%"
-                                                                                   , step = 1
-                                                                                   , post = " min"))
-                                  )
-                      ),
-    plotOutput("p")
-    )
 
+
+# USER INTERFACE ===============================================================
+ui <- page_sidebar(title = "UVizor"
+                   , sidebar = sidebar(title = NULL
+                                       , id = "SD"
+                                       , open = "open"
+                                       , width = "300px"
+                                       , resizable = TRUE
+                                       , sb_acc)
+                   , nav_set)
+
+# Server Logic =================================================================
 server <- function(input, output, session){
   output$p <- renderPlot({
-    rnorm(n = 500, mean = 0,sd = 2)|>
+    rnorm(n = 500, mean = 0, sd = 2)|>
       hist()
     })
   }
 
 
-# Run the application 
+# Run the application ========================================================= 
 shinyApp(ui = ui, server = server)
 
